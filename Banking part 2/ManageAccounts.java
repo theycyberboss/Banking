54403d2a033a1b778629892e90e250514788984b
theycyberboss <31087203+theycyberboss@users.noreply.github.com>
2018-11-29 11:39:43 -0500

    Add files via upload

---
+++ Banking part 2/ManageAccounts.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class ManageAccounts here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class ManageAccounts{
+    
+   public ManageAccounts()
+   {
+       
+   }
+ 
+   
+   public static void main(String[] args)
+   {
+       Account acc1, acc2;
+       
+       acc1 = new Account(1000,"Sally",1111);
+       acc2 = new Account(500,"Joe",1000);
+       System.out.println(acc1);
+       System.out.println(acc2);
+       
+       acc2.deposit(100);
+       System.out.println("Joe's Balance: " + acc2.getBalance());
+       acc1.withdraw(50);
+       System.out.println("Sally's Balance: " + acc1.getBalance());
+       
+       System.out.println("Joe's Balance: " + acc1.chargeFee());
+       System.out.println("Sally's Balance: " + acc2.chargeFee());
+       
+       acc2.changeName("Joseph");
+       
+       System.out.println(acc1);
+       System.out.println(acc2);
+   }
+   
+     
+   public String toString()
+   {
+       return "";
+   }
+   
+}
